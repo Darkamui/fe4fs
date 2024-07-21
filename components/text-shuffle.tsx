b43caa476dd269ed5579bc6550408c592a865e41
Daniel Jérusalmy <djerusal@uqac.ca>
2024-07-20 23:56:27 -0400

    initial commit + gh actions test

---
+++ components/text-shuffle.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {};
+
+export const TextShuffle = (props: Props) => {
+  return (
+    <div className="wrapper">
+      <div className="letters">
+        <span className="letter">F</span>
+        <span className="letter">E</span>
+        <span className="letter">4</span>
+        <span className="letter">F</span>
+        <span className="letter">S</span>
+      </div>
+    </div>
+  );
+};
